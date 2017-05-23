e653c7edcf7d04bc29816348bfc9e354de40522c
Sylvain Gravel <sgravel@lerondpoint.net>
2017-05-23 18:57:29 -0400

    Models

---
+++ FormsSkiaBikeTracker/FormsSkiaBikeTracker.Shared/Models/BaseModel.cs
@@ -0,0 +1,21 @@
+﻿// **********************************************************************
+// 
+//   BaseModel.cs
+//   
+//   This file is subject to the terms and conditions defined in
+//   file 'LICENSE.txt', which is part of this source code package.
+//   
+//   Copyright (c) 2017, Le rond-point
+// 
+// ***********************************************************************
+
+using PropertyChanged;
+
+namespace FormsSkiaBikeTracker.Models
+{
+    [ImplementPropertyChanged]
+    public class BaseModel
+    {
+        
+    }
+}
---
+++ FormsSkiaBikeTracker/FormsSkiaBikeTracker.Shared/Models/User.cs
@@ -0,0 +1,20 @@
+﻿// **********************************************************************
+// 
+//   User.cs
+//   
+//   This file is subject to the terms and conditions defined in
+//   file 'LICENSE.txt', which is part of this source code package.
+//   
+//   Copyright (c) 2017, Le rond-point
+// 
+// ***********************************************************************
+namespace FormsSkiaBikeTracker.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public string PicturePath { get; set; }
+    }
+}
