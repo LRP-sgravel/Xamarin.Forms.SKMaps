8a862add6fec443c679b56c81ad8a4422c9206ff
Sylvain Gravel <sgravel@lerondpoint.net>
2017-05-23 18:58:27 -0400

    Registered user list cell contents view

---
+++ FormsSkiaBikeTracker/FormsSkiaBikeTracker.Forms.UI/Controls/UserLoginControlView.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel;
+using FormsSkiaBikeTracker.Models;
+using FormsSkiaBikeTracker.Shared.ViewModels;
+using MvvmCross.Platform.WeakSubscription;
+using Xamarin.Forms;
+
+namespace FormsSkiaBikeTracker.Forms.UI.Controls
+{
+    public partial class UserLoginControlView
+    {
+        public static readonly BindableProperty UserProperty = BindableProperty.Create(nameof(User), typeof(User), typeof(UserLoginControlView), null, BindingMode.OneWay, null, UserPropertyChanged);
+
+        public User User
+        {
+            get { return (User)GetValue(UserProperty); }
+            set { SetValue(UserProperty, value); }
+        }
+
+        private UserLoginControlViewModel _internalViewModel;
+        public UserLoginControlViewModel InternalViewModel
+        {
+            get { return _internalViewModel; }
+            set
+            {
+                if (value != InternalViewModel)
+                {
+                    _internalViewModel = value;
+
+                    if (_internalViewModel != null)
+                    {
+                        _internalPropertychangedSubscription =
+                            _internalViewModel.WeakSubscribe<UserLoginControlViewModel, PropertyChangedEventArgs>("PropertyChanged", InternalVMPropertyChanged);
+                    }
+                    else
+                    {
+                        _internalPropertychangedSubscription = null;
+                    }
+
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private MvxWeakEventSubscription<UserLoginControlViewModel, PropertyChangedEventArgs> _internalPropertychangedSubscription;
+
+        public UserLoginControlView()
+        {
+            InitializeComponent();
+
+            _internalViewModel = new UserLoginControlViewModel();
+            InternalViewModel.Start();
+        }
+
+        private static void UserPropertyChanged(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            UserLoginControlView view = bindable as UserLoginControlView;
+
+            view.InternalViewModel.User = newvalue as User;
+        }
+
+        private void InternalVMPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == nameof(UserLoginControlViewModel.User))
+            {
+                User = InternalViewModel.User;
+            }
+        }
+    }
+}
---
+++ FormsSkiaBikeTracker/FormsSkiaBikeTracker.Shared/ViewModels/UserLoginControlViewModel.cs
@@ -0,0 +1,68 @@
+﻿// **********************************************************************
+// 
+//   UserLoginControlViewModel.cs
+//   
+//   This file is subject to the terms and conditions defined in
+//   file 'LICENSE.txt', which is part of this source code package.
+//   
+//   Copyright (c) 2017, Le rond-point
+// 
+// ***********************************************************************
+
+using FormsSkiaBikeTracker.Models;
+using LRPLib.Mvx.ViewModels;
+using SkiaSharp;
+
+namespace FormsSkiaBikeTracker.Shared.ViewModels
+{
+    public class UserLoginControlViewModel : LrpViewModel
+    {
+        private User _user;
+        public User User
+        {
+            get { return _user; }
+            set
+            {
+                if (User != value)
+                {
+                    _user = value;
+                    RaisePropertyChanged();
+
+                    if (User != null)
+                    {
+                        UserPicture = SKBitmap.Decode(new SKFileStream(User.PicturePath));
+                    }
+                    else
+                    {
+                        UserPicture = null;
+                    }
+                }
+            }
+        }
+
+        private SKBitmap _userPicture;
+        public SKBitmap UserPicture
+        {
+            get { return _userPicture; }
+            set
+            {
+                if (UserPicture != value)
+                {
+                    _userPicture = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public UserLoginControlViewModel()
+        {
+        }
+
+        public override void Start()
+        {
+            base.Start();
+            
+        }
+    }
+}
+
