597a2815b77409e0e1270f68e68628356037347d
Sylvain Gravel <sgravel@lerondpoint.net>
2017-05-25 01:02:11 -0400

    Updated LRPLib with improved Skia eperformance

---
+++ FormsSkiaBikeTracker/FormsSkiaBikeTracker.Forms.UI/Pages/SignUpPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿// **********************************************************************
+// 
+//   SignUpPage.xaml.cs
+//   
+//   This file is subject to the terms and conditions defined in
+//   file 'LICENSE.txt', which is part of this source code package.
+//   
+//   Copyright (c) 2017, Le rond-point
+// 
+// ***********************************************************************
+namespace FormsSkiaBikeTracker.Forms.UI.Pages
+{
+    public partial class SignUpPagePage
+    {
+        public SignUpPagePage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPropertyChanged(string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+        }
+    }
+}
---
+++ FormsSkiaBikeTracker/FormsSkiaBikeTracker.Shared/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,28 @@
+﻿// **********************************************************************
+// 
+//   SignUpPage.xaml.cs
+//   
+//   This file is subject to the terms and conditions defined in
+//   file 'LICENSE.txt', which is part of this source code package.
+//   
+//   Copyright (c) 2017, Le rond-point
+// 
+// ***********************************************************************
+using LRPLib.Mvx.ViewModels;
+
+namespace FormsSkiaBikeTracker.Shared.ViewModels
+{
+    public class SignUpViewModel : LrpViewModel
+    {
+        public SignUpViewModel()
+        {
+        }
+
+        public override void Start()
+        {
+            base.Start();
+            
+        }
+    }
+}
+
