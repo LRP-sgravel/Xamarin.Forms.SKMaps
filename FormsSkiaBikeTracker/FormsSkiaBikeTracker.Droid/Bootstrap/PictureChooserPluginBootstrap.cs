089658ed1e8c8a222b4801dc852315101ba035d6
Sylvain Gravel <sgravel@lerondpoint.net>
2017-05-26 10:19:32 -0400

    Signup screen layout

---
+++ FormsSkiaBikeTracker/FormsSkiaBikeTracker.Droid/Bootstrap/PictureChooserPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace FormsSkiaBikeTracker.Droid.Bootstrap
+{
+    public class PictureChooserPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.PictureChooser.PluginLoader>
+    {
+    }
+}
---
+++ FormsSkiaBikeTracker/FormsSkiaBikeTracker.iOS/Bootstrap/PictureChooserPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace FormsSkiaBikeTracker.iOS.Bootstrap
+{
+    public class PictureChooserPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.PictureChooser.PluginLoader, MvvmCross.Plugins.PictureChooser.iOS.Plugin>
+    {
+    }
+}
